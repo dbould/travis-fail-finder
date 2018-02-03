76e434a38c1a890ec6bff7e18eb898268847f56a
dave <dave@bould.net>
2018-02-03 09:56:18 +0000

    Switch repo_id fetch to use slug instead of branch.  Means we no longer need to loop through all of the repos for a user

--- TravisFailFinder/JobService.py
+++ TravisFailFinder/JobService.py
@@ -4,20 +4,14 @@ import json
 
 class JobService:
 
-    def get_repo_id(self, repo_name):
-        repo_id = 0
+    def get_repo_id(self, repo_slug):
 
         headers = {
             'Authorization': 'token fZ1gERYqeLtGNd0BesVmEg',
             'Travis-API-Version': '3',
         }
 
-        request = requests.get('https://api.travis-ci.org/repos', headers=headers)
-        repos = json.loads(request.text)
+        request = requests.get('https://api.travis-ci.org/repo/' + repo_slug, headers=headers)
+        repo = json.loads(request.text)
 
-        for repo in repos['repositories']:
-            if repo['name'] == repo_name:
-                repo_id = repo['id']
-                break
-
-        return repo_id
+        return repo['id']
