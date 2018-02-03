8ce06a07ed0d7cd78b9deca499aca8868b9b5429
dave <dave@bould.net>
2018-02-03 00:41:19 +0000

    Fill out get_repo_id

--- TravisFailFinder/JobService.py
+++ TravisFailFinder/JobService.py
@@ -1,4 +1,24 @@
+import requests
+import sys
+import json
+
+
 class JobService:
 
-    def test(self):
-        return "Hello World!"
+    def get_repo_id(self, repo_name):
+        repo_id = 0
+
+        headers = {
+            'Authorization': 'token fZ1gERYqeLtGNd0BesVmEg',
+            'Travis-API-Version': '3',
+        }
+
+        request = requests.get('https://api.travis-ci.org/repos', headers=headers)
+        repos = json.loads(request.text)
+
+        for repo in repos['repositories']:
+            if repo['name'] == repo_name:
+                repo_id = repo['id']
+                break
+
+        return repo_id
