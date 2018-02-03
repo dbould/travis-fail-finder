146e269f230090a750971cf39723c687bc298bf9
dave <dave@bould.net>
2018-02-03 22:02:14 +0000

    New method and corresponding test to get branch id

--- TravisFailFinder/JobService.py
+++ TravisFailFinder/JobService.py
@@ -15,3 +15,21 @@ class JobService:
         repo = json.loads(request.text)
 
         return repo['id']
+
+    def get_branch_id(self, repo_id, branch_name):
+
+        headers = {
+            'Authorization': 'token fZ1gERYqeLtGNd0BesVmEg',
+            'Travis-API-Version': '3',
+        }
+
+        request = requests.get('https://api.travis-ci.org/repo/' + repo_id + '/branches', headers=headers)
+        branches = json.loads(request.text)
+
+        branch_id = 0
+
+        for branch in branches['branches']:
+            if branch['name'] == branch_name:
+                branch_id = branch['last_build']['id']
+
+        return branch_id
--- test/test_job_service.py
+++ test/test_job_service.py
@@ -1,7 +1,12 @@
 from TravisFailFinder import JobService
 
+
 class TestTravisFailFinder:
 
     def test_get_repo_id(self):
         job_service = JobService()
         assert job_service.get_repo_id('dbould%2Ftravis-fail-finder') == 17456283
+
+    def test_get_branch_id(self):
+        job_service = JobService()
+        assert job_service.get_branch_id("17456283", 'master') == 336890330
