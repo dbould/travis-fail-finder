42747c65888ed9f196b2c0d0db7fe261fcaf9ffd
dave <dave@bould.net>
2018-02-03 00:56:42 +0000

    Switch test subject to be this repo

--- test/test_travis_fail_finder.py
+++ test/test_travis_fail_finder.py
@@ -4,4 +4,4 @@ class TestTravisFailFinder:
 
     def test_get_repo_id(self):
         job_service = JobService()
-        assert job_service.get_repo_id('davework') == 16045679
+        assert job_service.get_repo_id('travis-fail-finder') == 17456283
