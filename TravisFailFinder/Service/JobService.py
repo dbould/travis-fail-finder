2fab8e0c84da44861d2c84d2f697786d0062e228
dave <dave@bould.net>
2018-02-04 16:45:38 +0000

    Refactor to separate concerns

--- TravisFailFinder/Service/JobService.py
+++ TravisFailFinder/Service/JobService.py
@@ -24,22 +24,24 @@ class JobService:
 
     def get_branch_id(self, repo_id, branch_name):
         branches = self.get_request_response(self.base_url + 'repo/' + repo_id + '/branches')
-
-        branch_id = 0
-
-        for branch in branches['branches']:
-            if branch['name'] == branch_name:
-                branch_id = branch['last_build']['id']
+        branch_id = self.get_branch_id_from_branches(branches, branch_name)
 
         return branch_id
 
+    def get_branch_id_from_branches(self, branches, branch_name):
+        for branch in branches['branches']:
+            if branch['name'] == branch_name:
+                return branch['last_build']['id']
+
     def get_job_id(self, branch_id):
         jobs = self.get_request_response(self.base_url + 'build/' + branch_id + '/jobs')
-
-        for job in jobs['jobs']:
-            job_id = job['id']
+        job_id = self.get_job_id_from_jobs(jobs)
 
         return job_id
+
+    def get_job_id_from_jobs(self, jobs):
+        for job in jobs['jobs']:
+            return job['id']
 
     def get_log(self, job_id):
         log = self.get_request_response(self.base_url + 'job/' + job_id + '/log')
