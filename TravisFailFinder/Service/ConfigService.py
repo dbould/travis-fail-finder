cf29305db88e34fd60b9a6b1f7b2a56250c280ed
dave <dave@bould.net>
2018-02-04 21:02:07 +0000

    Fetch config from setup.json aplus test

--- TravisFailFinder/Service/ConfigService.py
+++ TravisFailFinder/Service/ConfigService.py
@@ -1,3 +1,11 @@
+import json
+import os
+
+
 class ConfigService:
+
     def get_config(self):
-        pass
+        with open(os.getcwd() + '/config/setup.json', 'r') as content_file:
+            config = content_file.read()
+
+            return json.loads(config)
--- test/test_config_service.py
+++ test/test_config_service.py
@@ -1,4 +1,9 @@
+from TravisFailFinder import ConfigService
+
+
 class TestConfigService:
 
     def test_get_config(self):
-        pass
+        config_service = ConfigService()
+        config = config_service.get_config()
+        assert config['repository_login'] == 'dbould'
