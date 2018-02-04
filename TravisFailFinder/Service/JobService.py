9ec6fd186f91e99113ef37710147c983e6d54893
dave <dave@bould.net>
2018-02-04 16:20:40 +0000

    Move re-used header code to separate method

--- TravisFailFinder/Service/JobService.py
+++ TravisFailFinder/Service/JobService.py
@@ -9,26 +9,20 @@ class JobService:
     def __init__(self, auth_token):
         self.auth_token = auth_token
 
-    def get_repo_id(self, repo_slug):
-
-        headers = {
+    def get_headers(self):
+        return {
             'Authorization': 'token ' + self.auth_token,
             'Travis-API-Version': '3',
         }
 
-        request = requests.get('https://api.travis-ci.org/repo/' + repo_slug, headers=headers)
+    def get_repo_id(self, repo_slug):
+        request = requests.get('https://api.travis-ci.org/repo/' + repo_slug, headers=self.get_headers())
         repo = json.loads(request.text)
 
         return repo['id']
 
     def get_branch_id(self, repo_id, branch_name):
-
-        headers = {
-            'Authorization': 'token ' + self.auth_token,
-            'Travis-API-Version': '3',
-        }
-
-        request = requests.get('https://api.travis-ci.org/repo/' + repo_id + '/branches', headers=headers)
+        request = requests.get('https://api.travis-ci.org/repo/' + repo_id + '/branches', headers=self.get_headers())
         branches = json.loads(request.text)
 
         branch_id = 0
@@ -40,13 +34,7 @@ class JobService:
         return branch_id
 
     def get_job_id(self, branch_id):
-
-        headers = {
-            'Authorization': 'token ' + self.auth_token,
-            'Travis-API-Version': '3',
-        }
-
-        request = requests.get('https://api.travis-ci.org/build/' + branch_id + '/jobs', headers=headers)
+        request = requests.get('https://api.travis-ci.org/build/' + branch_id + '/jobs', headers=self.get_headers())
         jobs = json.loads(request.text)
 
         for job in jobs['jobs']:
@@ -55,13 +43,7 @@ class JobService:
         return job_id
 
     def get_log(self, job_id):
-
-        headers = {
-            'Authorization': 'token ' + self.auth_token,
-            'Travis-API-Version': '3',
-        }
-
-        request = requests.get('https://api.travis-ci.org/job/' + job_id + '/log', headers=headers)
+        request = requests.get('https://api.travis-ci.org/job/' + job_id + '/log', headers=self.get_headers())
         log = json.loads(request.text)
 
         return log['content']
