7b457c792bbc2eff96bb93bdabc1a43c25b4bf9f
dave <dave@bould.net>
2018-02-06 07:32:04 +0000

    First mock test working, needs major tidy

--- test/Service/test_job_service.py
+++ test/Service/test_job_service.py
@@ -1,7 +1,44 @@
+import sys
+
+import os
+
 from TravisFailFinder import JobService
+import unittest
+from unittest import mock
 
 
 class TestTravisFailFinder:
+
+    def mocked_requests_get(*args, **kwargs):
+        class MockResponse:
+            text = ''
+
+            def __init__(self, json_data, status_code):
+                self.json_data = json_data
+                self.status_code = status_code
+                self.text = json_data
+
+            def json(self):
+                return self.json_data
+
+        if args[0] == 'https://api.travis-ci.org/repo/dbould%2Ftravis-fail-finder':
+            with open(os.getcwd() + '/test/Mocks/repo_response.json', 'r') as content_file:
+                return MockResponse(content_file.read(), 200)
+        elif args[0] == 'http://someotherurl.com/anothertest.json':
+            return MockResponse({"key2": "value2"}, 200)
+
+        return MockResponse(None, 404)
+
+    # We patch 'requests.get' with our own method. The mock object is passed in to our test case method.
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_fetch(self, mock_get):
+        # Assert requests.get calls
+        mgc = JobService().create()
+        json_data = mgc.get_repo_id('dbould%2Ftravis-fail-finder')
+        assert json_data == 17456283
+
+    if __name__ == '__main__':
+        unittest.main()
 
     def test_get_repo_id(self):
         job_service = JobService.create()
