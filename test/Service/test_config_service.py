040618101020fda127c2659aacf74a6ecb77e49e
dave <dave@bould.net>
2018-02-07 22:09:45 +0000

    Tests for the remaining config keys

--- test/Service/test_config_service.py
+++ test/Service/test_config_service.py
@@ -3,13 +3,46 @@ from TravisFailFinder import ConfigService
 
 class TestConfigService:
 
-    def test_get_config(self):
+    def test_get_config_repo(self):
+        config_service = ConfigService()
+        config = config_service.get_config()
+
+        expected = 0
+
+        if 'repository' in config:
+            expected = 1
+
+        assert expected == 1
+
+    def test_get_config_login(self):
         config_service = ConfigService()
         config = config_service.get_config()
 
         expected = 0
 
         if 'repository_login' in config:
+            expected = 1
+
+        assert expected == 1
+
+    def test_get_config_token(self):
+        config_service = ConfigService()
+        config = config_service.get_config()
+
+        expected = 0
+
+        if 'auth_token' in config:
+            expected = 1
+
+        assert expected == 1
+
+    def test_get_config_url(self):
+        config_service = ConfigService()
+        config = config_service.get_config()
+
+        expected = 0
+
+        if 'base_url' in config:
             expected = 1
 
         assert expected == 1
