db29f51bebbf4f0e3cc5af7fd0d6ce3007d06e5d
dave <dave@bould.net>
2018-02-07 09:13:38 +0000

    Fix test to just check if a key exists So tests pass on Travis and local without effecting local setup

--- test/Service/test_config_service.py
+++ test/Service/test_config_service.py
@@ -6,4 +6,10 @@ class TestConfigService:
     def test_get_config(self):
         config_service = ConfigService()
         config = config_service.get_config()
-        assert config['repository_login'] == 'dbould'
+
+        expected = 0
+
+        if 'repository_login' in config:
+            expected = 1
+
+        assert expected == 1
