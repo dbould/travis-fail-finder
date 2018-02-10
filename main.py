d7631cf6dfddc3613fce1da7ce6312849861c793
dave <dave@bould.net>
2018-02-10 17:20:08 +0000

    Playing with command line argument parsing

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import sys, getopt
+
 from TravisFailFinder import JobService
 from TravisFailFinder import LogService
+
+fullCmdArguments = sys.argv
+argumentList = fullCmdArguments[1:]
+
+unixOptions = "hb:v"
+gnuOptions = ["help", "branch", "verbose"]
+
+print(getopt.getopt(argumentList, unixOptions, gnuOptions))
+sys.exit()
 
 job_service = JobService().create()
 repo_id = job_service.get_repo_id('dbould%2Ftravis-fail-finder')
@@ -9,4 +20,4 @@ log = job_service.get_log(str(job_id))
 
 log_service = LogService()
 
-print(log_service.get_error_feedback(log['content']))
+print(log_service.get_error_feedback(log))
